6e66ad1f9debe0da4fd63bcfadc38cadf34dce2b
Rizal Lazuardi Firdaus <93332704+rzlzrdf@users.noreply.github.com>
2022-11-25 10:18:05 +0700

    done v.1.0

--- Components/Card.js
+++ Components/Card.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Card = (props) => {
   return (
     
-      <Link href={'/surah/' + props.id} id={props.id} className={'px-3 py-5 bg-gradient-to-tr from-slate-100 to-gray-200 hover:from-green-600 hover:to-teal-500 hover:text-white text-center rounded-3xl'}>
+      <Link href={'/surah/' + props.id} id={props.id} className={'px-3 py-5 bg-teal-900 shadow-xl text-white text-center rounded-3xl'}>
         <h2 className='text-3xl font-semibold font-serif'>{props.arab}</h2>
         <h1 className='text-lg'>{props.latin}</h1>
         <p className='text-xs italic'>{props.arti}</p>
