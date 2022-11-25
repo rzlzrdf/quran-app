859a388569a3145d35d9ba920a17e84bfdc51fd1
Rizal Lazuardi Firdaus <93332704+rzlzrdf@users.noreply.github.com>
2022-11-25 15:58:41 +0700

    add equran

--- Components/Card.js
+++ Components/Card.js
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 import React from 'react'
+import style from './Card.module.css'
 
 const Card = (props) => {
   return (
     
-      <Link href={'/surah/' + props.id} id={props.id} className={'px-3 py-5 bg-teal-900 shadow-xl text-white text-center rounded-3xl'}>
+      <Link href={'/surah/' + props.id} id={props.id} className={style.card}>
         <h2 className='text-3xl font-semibold font-serif'>{props.arab}</h2>
         <h1 className='text-lg'>{props.latin}</h1>
         <p className='text-xs italic'>{props.arti}</p>
