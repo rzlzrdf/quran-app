496de1b53e5b3448f385ff33a538ead25cc6e3fe
Rizal Lazuardi Firdaus <93332704+rzlzrdf@users.noreply.github.com>
2022-11-25 16:08:07 +0700

    add equran

--- Components/Footer.js
+++ Components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
      return (
           <footer className='basis-full h-fit p-0 text-center'>
                <h3 className='font-mono text-white'>Made by Rizal Lazuardi Firdaus ❤️</h3>
-               <Link href={'/credits'} className='font-serif text-yellow-300 font-bold text-lg'>See Credits</Link>
+               <Link href={'/credits'} style={{color:'yellow'}} className='font-serif text-yellow-300 font-bold text-lg'>See Credits</Link>
           </footer>
      )
 }
